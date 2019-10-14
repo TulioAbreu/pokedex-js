6a11edb9cf689c79fb4af375f58e6881a7046729
TulioAbreu <marco-tulioabreu1@hotmail.com>
2019-10-14 14:09:25 -0300

    Implement loading animation

--- frontend/src/pages/Pokemon.js
+++ frontend/src/pages/Pokemon.js
@@ -2,44 +2,53 @@ import React, { useEffect, useState } from 'react';
 import api from '../services/api'
 import './Pokemon.css';
 import './Main.css'
+import FadeIn from "react-fade-in";
+import Lottie from "react-lottie";
+import ReactLoading from "react-loading";
 
 export default function Pokemon({ history, match }) {
     const [reloadPage, setReload] = useState(true);
     const [pokename, setPokename] = useState('');
     const [types, setTypes] = useState([])
     const [pokeInfo, setPokeInfo ] = useState({});
+    const [isLoading, setIsLoading] = useState(true);
 
     async function handleSubmit(e) {
         e.preventDefault();
+        setIsLoading(true)
         history.push(`/poke/${pokename}`);
         setReload(true);
     }
-
     
+    async function loadPokemon() {
+        const pokename = match.params.pokeData
+        const response = await api.post(`/poke/${pokename}`)
+
+        const typesList = response.data.type
+        let pokeTypeStyles = []
+        for (let i = 0; i < typesList.length; ++i) {
+            pokeTypeStyles.push({
+                str: typesList[i],
+                cssClass: "type-icon type-" + typesList[i].toLowerCase()
+            })
+        }
+
+        setTypes(pokeTypeStyles)
+        setPokeInfo(response.data)
+
+        
+    }
+
+    async function loadPokemonPage() {
+        await loadPokemon().then(_ => {
+            setIsLoading(false)
+        })
+    }
 
     useEffect(() => {
-        if (reloadPage) {
-            async function loadPokemon() {
-                const pokename = match.params.pokeData;
-                const response = await api.post(`/poke/${pokename}`);
-
-                const typesList = response.data.type
-                let pokeTypeStyles = []
-                for (let i = 0; i < typesList.length; ++i) {
-                    pokeTypeStyles.push({
-                        str: typesList[i],
-                        cssClass: "type-icon type-" + typesList[i].toLowerCase()
-                    })
-                }
-                setTypes(pokeTypeStyles)
-
-                setPokeInfo(response.data);
-            }
-    
-
-            loadPokemon();
-            setReload(false);
-        }
+        if (! reloadPage) return
+        loadPokemonPage();
+        setReload(false);
     });
 
     return (
@@ -54,25 +63,36 @@ export default function Pokemon({ history, match }) {
                 </input>
                 <button 
                     type="submit"
-                    className="PokeButton">
+                    className="PokeButton"
+                >
                     Search
                 </button>
             </form>
 
-            <div className="pokeTable">
-                <p className="pokeName">{pokeInfo.number} - {pokeInfo.name}</p>
-                <img className="pokeImage" 
-                    src={pokeInfo.imgURL} 
-                    alt={pokeInfo.name} width="300"></img>
-                {types.map(type => (
-                    <div className={ type.cssClass }>
-                        { type.str }
+            <div className={isLoading? "loading":"notLoading"}></div>
+
+            {isLoading? 
+                <div className="loadingBars">
+                    <ReactLoading type={"bars"} color={"white"} />
+                </div>
+                :
+                <div>
+                    <div className="pokeTable">
+                        <p className="pokeName">{pokeInfo.number} - {pokeInfo.name}</p>
+                        <img className="pokeImage" 
+                            src={pokeInfo.imgURL} 
+                            alt={pokeInfo.name} width="300"></img>
+                        {types.map(type => (
+                            <div className={ type.cssClass }>
+                                { type.str }
+                            </div>
+                        ))}
+                        <p>Height: {pokeInfo.height}</p>
+                        <p>Weight: {pokeInfo.weight}</p>
+                        <p className="pokeBiology">Biology: {pokeInfo.biology}</p>
                     </div>
-                ))}
-                <p>Height: {pokeInfo.height}</p>
-                <p>Weight: {pokeInfo.weight}</p>
-                <p className="pokeBiology">Biology: {pokeInfo.biology}</p>
-            </div>
+                </div>
+            }
         </div>
     )
 }
